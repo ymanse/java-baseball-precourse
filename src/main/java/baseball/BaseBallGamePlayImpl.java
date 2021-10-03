2b30494ca495806214de1a8cd249b517b8ec7b95
ymanse <ymanse@naver.com>
2021-10-03 21:52:01 +0900

    게임 플레이 초기화 구현 게임 시작시 balls를 Random 값으로 3개 초기화

--- src/main/java/baseball/BaseBallGamePlayImpl.java
+++ src/main/java/baseball/BaseBallGamePlayImpl.java
@@ -1,8 +1,23 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseBallGamePlayImpl implements IGamePlay {
+  static final Integer MinBall = 1;
+  static final Integer MaxBall = 9;
+  Map<Integer, Integer> balls = new HashMap<>();
+
   @Override
-  public void init() {  }
+  public void init() {
+    balls.clear();
+    while (balls.size() < 3) {
+      Integer ball = Randoms.pickNumberInRange(MinBall, MaxBall);
+      balls.putIfAbsent(ball, balls.size() + 1);
+    }
+  }
 
   @Override
   public Boolean runPlay() {
