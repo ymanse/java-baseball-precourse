aec02b7b7de2e6042c4adf02d7d1a29dd324cf04
ymanse <ymanse@naver.com>
2021-10-04 12:24:30 +0900

    PlayeResult 코드 정렬

--- src/main/java/baseball/PlayResult.java
+++ src/main/java/baseball/PlayResult.java
@@ -1,20 +1,22 @@
 package baseball;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import static baseball.BaseBallGameConst.MAX_BALL_COUNT;
 
 public class PlayResult {
   private Integer strikeResult = 0;
   private Integer ballResult = 0;
+
   public PlayResult(Map<Integer, Integer> intputBalls, Map<Integer, Integer> balls) {
-    for(Map.Entry<Integer, Integer> elem: intputBalls.entrySet()){
-      if(balls.containsKey(elem.getKey()) && balls.get(elem.getKey()).equals(elem.getValue())){
+    for (Map.Entry<Integer, Integer> elem : intputBalls.entrySet()) {
+      if (balls.containsKey(elem.getKey()) && balls.get(elem.getKey()).equals(elem.getValue())) {
         strikeResult++;
         continue;
       }
-      if(balls.containsKey(elem.getKey())){
+      if (balls.containsKey(elem.getKey())) {
         ballResult++;
         continue;
       }
@@ -27,8 +29,12 @@ public class PlayResult {
 
   public String getResultString() {
     List<String> resultStrList = new ArrayList<>();
-    if(strikeResult > 0) resultStrList.add(String.format("%d스트라이크", strikeResult));
-    if(ballResult > 0) resultStrList.add(String.format("%d볼", ballResult));
+    if (strikeResult > 0) {
+      resultStrList.add(String.format("%d스트라이크", strikeResult));
+    }
+    if (ballResult > 0) {
+      resultStrList.add(String.format("%d볼", ballResult));
+    }
     if (resultStrList.isEmpty()) {
       return "낫싱";
     }
